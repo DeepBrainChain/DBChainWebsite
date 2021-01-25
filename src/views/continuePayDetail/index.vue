d24ca59002ca559ac7a6839987e55431ec804f94
enochzhao <zypsalms@sina.com>
2021-01-25 16:26:16 +0800

    修改页面报错问题，为页面添加头部和页脚

--- src/views/continuePayDetail/index.vue
+++ src/views/continuePayDetail/index.vue
@@ -1,110 +1,178 @@
 <template>
-  <div class="bg-box wrap1440 mb100">
-    <div class="border-box">
-      <div class="title">
-        <h3>{{$t('continue_pay_details')}}</h3>
-      </div>
-
-      <div v-for="item in content" class="border-content">
-        <div class="tools-head">
-          <div class="l-wrap">
-            <!--          <span class="tools-title">{{$t('gpu.mcStatusTitle')}}：<b>{{$t('gpu.machineOnLine')}}</b></span>-->
-            <span>ID:{{item.continue_pay_order_id}}</span>
-
-            <span
-              v-if="item.pay_success && item.rest_dbc_count>0 &&!item.main_order_is_over"
-              class="tools-title"
-            >&nbsp; &nbsp; &nbsp; &nbsp;{{$t('gpu.remainingTime')}}：{{$secToDate(item.rest_time_rent*60, 'DHM')}}</span>
-          </div>
-          <div class="r-wrap">
-            <span v-if="item.order_is_cancer">{{$t('my_machine_order_cancer')}}</span>
-            <span
-              v-else-if="item.rest_dbc_count===0 ||item.main_order_is_over"
-            >{{$t('continue_pay_order_is_over')}}</span>
-            <span v-else-if="item.pay_success ">{{$t('continue_pay_success')}}</span>
-            <span
-              v-else-if="item.pay_error && !item.return_dbc"
-            >{{$t('my_machine_order_pay_error')}}</span>
-            <span
-              v-else-if="item.pay_error && item.return_dbc"
-            >{{$t('my_machine_order_return_dbc')}}</span>
-          </div>
+  <div>
+    <Header
+      class="header"
+      v-if="this.$store.state.webtype"
+      :underlineStyle="underlineStyle"
+    />
+    <div class="bg-box wrap1440 mb100">
+      <div class="border-box" ref="boxObject">
+        <div class="title">
+          <h3>{{ $t("continue_pay_details") }}</h3>
         </div>
-        <div class="pay-wrap">
-          <div>
-            <span
-              class="td"
-            >{{$t('continue_pay_time')}}：{{parseInt(item.rent_time_length/60)}}{{$t('my_machine_hour')}}{{item.rent_time_length%60}}{{$t('my_machine_min')}}</span>
-            <span
-              class="td"
-            >{{$t('my_machine_beused_time')}}：{{parseInt(item.real_rent_time/60)}}{{$t('my_machine_hour')}}{{item.real_rent_time%60}}{{$t('my_machine_min')}}</span>
-            <span class="td">{{$t('gpu.payDBCs')}}：{{item.dbc_total_count}} DBC</span>
-          </div>
-          <div>
-            <span class="td">{{$t('gpu.actualPrice')}}：{{item.dbc_real_need_count}} DBC</span>
-            <span
-              class="td"
-            >{{$t('gpu.gpuBilling')}}：$ {{item.gpu_price_dollar}}/{{$t('my_machine_hour')}}</span>
-            <span class="td">{{$t('gpu.payPrice')}}：$ {{item.dbc_price.toFixed(4)}}</span>
-          </div>
-        </div>
-        <div class="tools-head">
-          <div class="l-wrap">
-            <span v-if="item.vocing === true">{{$t('myMachine_is_pay_vocing')}}</span>
 
-            <span
-              class="cRed"
-              v-else-if="isPaying&& !item.order_is_cancer&& !item.pay_success&& !item.pay_error"
-            >{{$t('myMachine_is_dbc_transfering')}}</span>
-            <span
-              class="cRed"
-              v-else-if="local_pay_error && !isPaying"
-            >{{$t('myMachine_is_transfer_error')}}</span>
-            <span
-              class="cRed"
-              v-else-if="item.vocing_pay && !item.order_is_cancer&& !item.pay_success&& !item.pay_error"
-            >{{$t('my_machine_order_vocing_pay')}}</span>
-            <span
-              v-else-if="item.pay_success===false&& item.order_is_cancer===false&&item.pay_error===false "
-            >{{$t('myMachine_confirm_pay_tip')}}</span>
+        <div v-for="item in content" class="border-content">
+          <div class="tools-head">
+            <div class="l-wrap">
+              <!--          <span class="tools-title">{{$t('gpu.mcStatusTitle')}}：<b>{{$t('gpu.machineOnLine')}}</b></span>-->
+              <span>ID:{{ item.continue_pay_order_id }}</span>
+
+              <span
+                v-if="
+                  item.pay_success &&
+                  item.rest_dbc_count > 0 &&
+                  !item.main_order_is_over
+                "
+                class="tools-title"
+                >&nbsp; &nbsp; &nbsp; &nbsp;{{ $t("gpu.remainingTime") }}：{{
+                  $secToDate(item.rest_time_rent * 60, "DHM")
+                }}</span
+              >
+            </div>
+            <div class="r-wrap">
+              <span v-if="item.order_is_cancer">{{
+                $t("my_machine_order_cancer")
+              }}</span>
+              <span
+                v-else-if="item.rest_dbc_count === 0 || item.main_order_is_over"
+                >{{ $t("continue_pay_order_is_over") }}</span
+              >
+              <span v-else-if="item.pay_success">{{
+                $t("continue_pay_success")
+              }}</span>
+              <span v-else-if="item.pay_error && !item.return_dbc">{{
+                $t("my_machine_order_pay_error")
+              }}</span>
+              <span v-else-if="item.pay_error && item.return_dbc">{{
+                $t("my_machine_order_return_dbc")
+              }}</span>
+            </div>
           </div>
-          <div v-if="item.pay_success===false&& item.order_is_cancer===false " class="r-wrap">
-            <template>
+          <div class="pay-wrap">
+            <div>
+              <span class="td"
+                >{{ $t("continue_pay_time") }}：{{
+                  parseInt(item.rent_time_length / 60)
+                }}{{ $t("my_machine_hour") }}{{ item.rent_time_length % 60
+                }}{{ $t("my_machine_min") }}</span
+              >
+              <span class="td"
+                >{{ $t("my_machine_beused_time") }}：{{
+                  parseInt(item.real_rent_time / 60)
+                }}{{ $t("my_machine_hour") }}{{ item.real_rent_time % 60
+                }}{{ $t("my_machine_min") }}</span
+              >
+              <span class="td"
+                >{{ $t("gpu.payDBCs") }}：{{ item.dbc_total_count }} DBC</span
+              >
+            </div>
+            <div>
+              <span class="td"
+                >{{ $t("gpu.actualPrice") }}：{{
+                  item.dbc_real_need_count
+                }}
+                DBC</span
+              >
+              <span class="td"
+                >{{ $t("gpu.gpuBilling") }}：$ {{ item.gpu_price_dollar }}/{{
+                  $t("my_machine_hour")
+                }}</span
+              >
+              <span class="td"
+                >{{ $t("gpu.payPrice") }}：$
+                {{ item.dbc_price.toFixed(4) }}</span
+              >
+            </div>
+          </div>
+          <div class="tools-head">
+            <div class="l-wrap">
+              <span v-if="item.vocing === true">{{
+                $t("myMachine_is_pay_vocing")
+              }}</span>
+
+              <span
+                class="cRed"
+                v-else-if="
+                  isPaying &&
+                  !item.order_is_cancer &&
+                  !item.pay_success &&
+                  !item.pay_error
+                "
+                >{{ $t("myMachine_is_dbc_transfering") }}</span
+              >
+              <span class="cRed" v-else-if="local_pay_error && !isPaying">{{
+                $t("myMachine_is_transfer_error")
+              }}</span>
+              <span
+                class="cRed"
+                v-else-if="
+                  item.vocing_pay &&
+                  !item.order_is_cancer &&
+                  !item.pay_success &&
+                  !item.pay_error
+                "
+                >{{ $t("my_machine_order_vocing_pay") }}</span
+              >
+              <span
+                v-else-if="
+                  item.pay_success === false &&
+                  item.order_is_cancer === false &&
+                  item.pay_error === false
+                "
+                >{{ $t("myMachine_confirm_pay_tip") }}</span
+              >
+            </div>
+            <div
+              v-if="
+                item.pay_success === false && item.order_is_cancer === false
+              "
+              class="r-wrap"
+            >
+              <template>
+                <el-button
+                  v-if="item.pay_error === false"
+                  :disabled="item.vocing_pay"
+                  plain
+                  :loading="isPaying"
+                  class="tool-btn"
+                  size="mini"
+                  @click="payOrder(item)"
+                  >{{ $t("myMachine_confirm_pay") }}</el-button
+                >
+                <el-button
+                  v-if="!item.vocing_pay && item.pay_error === false"
+                  :disabled="isPaying"
+                  class="tool-btn"
+                  size="mini"
+                  plain
+                  @click="cancelOrder(item)"
+                  >{{ $t("myMachine_concer_order") }}</el-button
+                >
+              </template>
               <el-button
-                v-if="item.pay_error===false"
-                :disabled="item.vocing_pay"
-                plain
-                :loading="isPaying"
+                v-if="
+                  item.return_dbc === false &&
+                  item.pay_error === true &&
+                  !item.main_order_is_over
+                "
                 class="tool-btn"
-                size="mini"
-                @click="payOrder(item)"
-              >{{$t('myMachine_confirm_pay')}}</el-button>
-              <el-button
-                v-if="!item.vocing_pay &&item.pay_error===false"
-                :disabled="isPaying"
-                class="tool-btn"
-                size="mini"
+                style="width: 86px"
                 plain
-                @click="cancelOrder(item)"
-              >{{$t('myMachine_concer_order')}}</el-button>
-            </template>
-            <el-button
-              v-if="item.return_dbc === false &&item.pay_error===true&&!item.main_order_is_over"
-              class="tool-btn"
-              style="width: 86px"
-              plain
-              size="mini"
-              @click="openReturnDbc(item)"
-            >{{$t('myMachine_return_dbc')}}</el-button>
+                size="mini"
+                @click="openReturnDbc(item)"
+                >{{ $t("myMachine_return_dbc") }}</el-button
+              >
+            </div>
           </div>
         </div>
       </div>
+      <dlg-return-dbc-continue
+        :open.sync="dlgReturnDbc_open"
+        :item="curItem"
+        @success="returnSuccess"
+      ></dlg-return-dbc-continue>
     </div>
-    <dlg-return-dbc-continue
-      :open.sync="dlgReturnDbc_open"
-      :item="curItem"
-      @success="returnSuccess"
-    ></dlg-return-dbc-continue>
+    <Footer v-if="this.$store.state.webtype" />
   </div>
 </template>
 
@@ -116,14 +184,18 @@ import {
   continue_pay_cancer_order_code,
   continue_pay_cancer_order,
   continue_pay_return_dbc,
-  continue_pay_return_dbc_code
+  continue_pay_return_dbc_code,
 } from "@/api";
 import { transfer } from "@/utlis";
 import DlgReturnDbcContinue from "@/components/machine/dlg_returnDbcContinue";
+import Header from "@/congTuCloud/components/header/SubHeader.vue";
+import Footer from "@/congTuCloud/components/footer/Footer.vue";
 export default {
   name: "continuePayDetail",
   components: {
-    DlgReturnDbcContinue
+    DlgReturnDbcContinue,
+    Header,
+    Footer,
   },
   data() {
     return {
@@ -138,7 +210,11 @@ export default {
       queryOrderListSi: undefined,
       dlgReturnDbc_open: false,
       si: undefined,
-      curItem: undefined
+      curItem: undefined,
+      underlineStyle: {
+        width: "65px",
+        left: "218px",
+      },
     };
   },
   created() {
@@ -158,6 +234,12 @@ export default {
       clearInterval(this.si);
     }
   },
+  mounted() {
+    if (this.$t("website_name") === "congTuCloud") {
+      this.$refs.boxObject.style.marginBottom = "0px";
+      this.$refs.boxObject.style.minHeight = "100vh";
+    }
+  },
   computed: {
     //   rateList() {
     //     content.map(item => {
@@ -166,6 +248,7 @@ export default {
     //   }
   },
   methods: {
+    returnSuccess() {},
     // 退币
     openReturnDbc(item) {
       this.curItem = item;
@@ -175,16 +258,16 @@ export default {
       continue_pay_cancer_order_code({
         continue_pay_order_id: item.continue_pay_order_id,
         user_name_platform: this.$t("website_name"),
-        language: this.$i18n.locale
+        language: this.$i18n.locale,
       })
-        .then(res => {
+        .then((res) => {
           if (res.status === 1) {
             this.$prompt(
               this.$t("myMachine_code_send"),
               this.$t("myMachine_cancer_order"),
               {
                 confirmButtonText: this.$t("myMachine_confirm"),
-                cancelButtonText: this.$t("myMachine_cancer")
+                cancelButtonText: this.$t("myMachine_cancer"),
               }
             )
               .then(({ value }) => {
@@ -192,26 +275,26 @@ export default {
                   continue_pay_order_id: item.continue_pay_order_id,
                   cancer_code: value,
                   user_name_platform: this.$t("website_name"),
-                  language: this.$i18n.locale
+                  language: this.$i18n.locale,
                 });
               })
-              .then(res => {
+              .then((res) => {
                 if (res.status === 1) {
                   this.$message({
                     showClose: true,
                     message: res.msg,
-                    type: "success"
+                    type: "success",
                   });
                   this.queryContinuePayDetail();
                 } else {
                   this.$message({
                     showClose: true,
                     message: res.msg,
-                    type: "error"
+                    type: "error",
                   });
                 }
               })
-              .catch(err => {
+              .catch((err) => {
                 if (err) {
                   console.log(err);
                 }
@@ -220,17 +303,17 @@ export default {
             this.$message({
               showClose: true,
               message: res.msg,
-              type: "error"
+              type: "error",
             });
             return Promise.reject();
           }
         })
-        .then(res => {
+        .then((res) => {
           if (res.status === 1) {
             this.$message({
               showClose: true,
               message: res.msg,
-              type: "success"
+              type: "success",
             });
           }
         })
@@ -247,8 +330,8 @@ export default {
       get_dbchain_address({
         order_id: item.order_id,
         user_name_platform,
-        language
-      }).then(res => {
+        language,
+      }).then((res) => {
         if (res.status === 1 && res.content) {
           const amount = item.dbc_total_count + item.code * 1;
           this.$confirm(
@@ -256,7 +339,7 @@ export default {
             this.$t("myMachine_please_confirm_pay"),
             {
               confirmButtonText: this.$t("myMachine_confirm"),
-              cancelButtonText: this.$t("myMachine_cancer")
+              cancelButtonText: this.$t("myMachine_cancer"),
             }
           )
             .then(({ value }) => {
@@ -264,10 +347,10 @@ export default {
               this.local_pay_error = false;
               return transfer({
                 toAddress: res.content,
-                amount
+                amount,
               });
             })
-            .then(res => {
+            .then((res) => {
               if (res.status === 1) {
                 console.log("转账成功");
                 const txid = res.response.txid;
@@ -282,8 +365,8 @@ export default {
                   continue_pay_order_id: item.continue_pay_order_id,
                   dbc_hash: txid,
                   user_name_platform,
-                  language
-                }).then(res => {
+                  language,
+                }).then((res) => {
                   item.vocing_pay = true;
                   this.queryContinuePayDetail();
                   if (res.status === 1) {
@@ -300,9 +383,9 @@ export default {
                     continue_pay_order_id: item.continue_pay_order_id,
                     dbc_hash: txid,
                     user_name_platform,
-                    language
+                    language,
                   })
-                    .then(res => {
+                    .then((res) => {
                       item.vocing_pay = true;
                       this.queryContinuePayDetail();
                       if (res.status === 1) {
@@ -311,13 +394,13 @@ export default {
                         item.vocing_pay = false;
                       }
                     })
-                    .catch(err => {
+                    .catch((err) => {
                       if (err && err.status === -1) {
                         console.log(err.msg);
                         this.$message({
                           showClose: true,
                           message: err.msg,
-                          type: "error"
+                          type: "error",
                         });
                         clearInterval(this.si);
                       } else if (err && err.status === -2) {
@@ -341,7 +424,7 @@ export default {
         } else {
           return Promise.reject({
             status: -1,
-            msg: "出现未知错误，无法续租"
+            msg: "出现未知错误，无法续租",
           });
         }
       });
@@ -361,18 +444,21 @@ export default {
       get_continue_pay_order_list({
         order_id: this.$route.query.order_id,
         user_name_platform,
-        language
-      }).then(res => {
+        language,
+      }).then((res) => {
         if (res.status === 1) {
           this.content = res.content;
         }
       });
-    }
-  }
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
+.header {
+  margin-bottom: 30px;
+}
 .rate-wrap {
   label {
     vertical-align: top;
