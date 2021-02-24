ed71e5a42881bae82e1fd64fd476fcefd0b54af6
enochzhao <zypsalms@sina.com>
2021-02-24 18:18:35 +0800

    修改展示机器页每页可展示机器的数量

--- src/views/gpu/list.vue
+++ src/views/gpu/list.vue
@@ -667,7 +667,7 @@
         @size-change="handleSizeChange"
         @current-change="handleCurrentChange"
         :current-page="currentPage"
-        :page-sizes="[10, 20, 50, 100]"
+        :page-sizes="[5, 10, 20, 50]"
         :page-size="pageSize"
         layout="total, sizes, prev, pager, next, jumper"
         :total="machineCount"
