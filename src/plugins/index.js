8bdc01079dc1519bcde9debccf7615f9f7480cac
Han <2414221668@qq.com>
2021-06-02 22:12:22 +0800

    修改移动端不显示问题

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -1291,7 +1291,7 @@ export default function install(Vue, options) {
   Vue.prototype.$alert = MessageBox.alert
   Vue.prototype.$confirm = MessageBox.confirm
   Vue.prototype.$prompt = MessageBox.prompt
-  Vue.prototype.$notify = Notification
+  // Vue.prototype.$notify = Notification
   Vue.prototype.$message = Message
   Vue.prototype.$secToDate = (msd, format = 'DHMS') => {
     var time = msd
